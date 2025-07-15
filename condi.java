716fd29fb5543d2c5425b0b8f85e80b2a28b4288
Nitesh Kumar <kumar945153@gmail.com>
2025-07-15 15:14:15 +0530

    intital commit

---
+++ condi.java
@@ -0,0 +1,23 @@
+//if-else statement
+public class condi {
+    public static void main(String[] args) {
+        int marks=95;
+        if(marks>=90)
+        {
+        System.out.println("Grade A");
+        }
+        else if(marks>=70)
+        {
+        System.out.println("Grade B");
+        }
+        else if(marks>=50){
+        System.out.println("Grade C");
+        }
+        else if(marks>=35){
+        System.out.println("Grade D");
+        }
+        else{
+        System.out.print("Fail");
+        }
+    }
+}
